9301f41d93a95af2e46a23f0e48097afa9b7dba6
natalia <ntla.brs@gmail.com>
2022-09-27 15:00:39 -0300

    Primeiro requisito

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(primeiro, segundo) {
+  if (primeiro === true && segundo === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
 
 // Desafio 2
 function calcArea() {
