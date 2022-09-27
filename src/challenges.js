aed9377c49944c4d5be94275250d7d81853d9c5b
natalia <ntla.brs@gmail.com>
2022-09-27 15:07:35 -0300

    Segundo requisito

--- src/challenges.js
+++ src/challenges.js
@@ -7,11 +7,12 @@ function compareTrue(primeiro, segundo) {
   }
 }
 
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+    let calc = (base * height) / 2;
+  return calc
 }
+console.log (calcArea(10,50))
 
 // Desafio 3
 function splitSentence() {
